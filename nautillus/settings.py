c15f78a5a095c18b2581122742288ddb2063c411
sikha10 <kinggurieli@gmail.com>
2023-02-21 14:40:47 +0400

    make user register

--- nautillus/settings.py
+++ nautillus/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'courses',
     "corsheaders",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -181,3 +182,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+AUTH_USER_MODEL = 'users.NewUser'
--- nautillus/urls.py
+++ nautillus/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('courses.urls', namespace='courses_api')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
