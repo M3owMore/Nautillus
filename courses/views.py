4bd78f532d116e764f9aa2c13aab47cb5db18ffe
sikha10 <kinggurieli@gmail.com>
2023-03-13 13:11:49 +0400

    add caching with redis

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from django.core.cache import cache
 
 
 
@@ -23,8 +24,20 @@ class CourseDetail(generics.RetrieveAPIView):
 
     def get_object(self, queryset=None, **kwargs):
         item = self.kwargs.get('pk')
-        return get_object_or_404(Course, title=item)
-
+        if cache.get(item):
+            course = cache.get(item)
+            print("hit the cache")
+        else:
+            try:
+                course = get_object_or_404(Course, title=item)
+                cache.set(item, course)
+                print('hit the DB')
+            except:
+                return status.HTTP_404_NOT_FOUND
+            
+        return course
+    
+    
 class CourseEdit(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAdminUser]
     queryset = Course.objects.all()
--- nautillus/settings.py
+++ nautillus/settings.py
@@ -214,3 +214,13 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kinggurieli@gmail.com'
 EMAIL_HOST_PASSWORD = 'ghytakfiqzvlojtu'
 EMAIL_USE_TLS = True
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
