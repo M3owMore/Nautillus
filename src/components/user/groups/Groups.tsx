d04fdb7a9c62d7d2fec5a51d6ce208b0e3c14893
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-07-17 21:37:56 +0400

    Add files via upload

---
+++ src/components/user/groups/Groups.tsx
@@ -0,0 +1,48 @@
+import { useState} from 'react';
+import Header from '../../Header';
+import GroupsList from './GroupsList';
+import Chat from '../Chat';
+import friendsJson from "../../../friends.json"
+import useWindowDimensions from '../../../js/useWindowDimensions';
+
+const Groups = (props:any) => {
+    const { width } = useWindowDimensions();
+
+    let friendName = ""
+
+    if(friendsJson.length > 0){
+        friendName = friendsJson[0].name
+    }
+
+    const [name, setName] = useState(friendName)
+    const [isChat, setIsChat] = useState(false)
+
+
+
+    return ( 
+        <div className='w-screen h-screen flex flex-col'>
+            <Header app={props.app} scroll = {false} fixed={true}/>
+            <div className='w-full h-full flex flex-row'>
+                {(width >= 550) 
+                ? 
+                    <>
+                        <div className='w-64 h-full'><GroupsList app={props.app} name={name} setName={setName}/></div>
+                        <Chat app={props.app} name={name} setName={setName}/>
+                    </>
+                : 
+                    <>
+                        {isChat 
+                        ? 
+                            <Chat app={props.app} name={name} setName={setName} setIsChat={setIsChat}/>
+                        :
+                            <div className='w-full h-full'><GroupsList app={props.app} name={name} setName={setName} setIsChat={setIsChat}/></div>
+                        }   
+                    </>
+                }
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Groups;
