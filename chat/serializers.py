fe753db77a4da2b626f17860ec3309521fdcf38e
sikha10 <111437894+sikha10@users.noreply.github.com>
2023-08-03 22:04:03 +0400

    Delete chat directory

--- chat/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Room
-from django.conf import settings
-
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ('__all__')
