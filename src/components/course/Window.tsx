b5d7fcd3cf548d5520a056d20413553ed96308e9
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-06 22:51:46 +0400

    Delete src directory

--- src/components/course/Window.tsx
+++
@@ -1,9 +0,0 @@
-const Window = (props:any) => {
-    return (
-        <div className="p-2 bg-neutral-900 rounded-lg">
-            {props.children}
-        </div>
-    );
-}
-
-  export default Window;
