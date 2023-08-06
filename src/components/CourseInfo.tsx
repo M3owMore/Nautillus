fb6ab63aad896fdafa3d35baf4f20863d803ab5c
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-06 22:54:53 +0400

    Add files via upload

---
+++ src/components/CourseInfo.tsx
@@ -0,0 +1,49 @@
+import Header from "./Header";
+import Footer from "./Footer"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const CourseInfo = (props:any) => {
+    const [course, setCourse]:any = useState(null)
+    let courseName:any = "";
+
+    useEffect(() => {
+        async function fetchData() {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            courseName = urlParams.get("course");
+
+            const res = await axios.get(props.app.backendURL + "" + courseName, {headers: {Authorization: 'Bearer ' + props.app.cookies.get('access')}});
+
+            setCourse(res.data)
+        };
+        
+        fetchData();
+    }, []);
+  return (
+    <div className="flex flex-col w-screen h-screen items-stretch">
+        <Header scroll={false} fixed={false} app={props.app}/>
+        <br/>
+        <div className="flex flex-col items-center justify-center h-screen gap-2">
+            {course !== null 
+            ?
+              <>
+                <h1 className="text-main text-5xl text-center">{course.name}</h1>
+                <p className="text-zinc-400 text-xl text-center">Level: {course.level}</p>
+                <p className="text-main text-2xl text-center m-1">{course.description}</p>
+                <ul className="text-main list-disc ml-4">
+                    {course.split("$").map((object:any, i:any) => (<li key={i} className="text-main text-md text-left ml-4 m-2">{object}</li>))}
+                </ul>
+                <p className="text-zinc-400 text-sm text-center m-1"><span className="text-main font-bold text-3xl">10$</span>/month</p>
+                <button className="bg-main text-black text-2xl font-bold py-1 px-6 rounded-md m-4 hover:bg-zinc-300 duration-200">Buy</button>
+              </>
+            : null
+            }
+            
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default CourseInfo
---
+++ src/components/user/Nav.tsx
@@ -0,0 +1,41 @@
+import NavBut from "./NavBut";
+import useWindowDimensions from '../../js/useWindowDimensions';
+import { FaUser,FaUserFriends } from 'react-icons/fa';
+import { HiUserGroup, HiAcademicCap } from 'react-icons/hi';
+import { RiComputerFill } from 'react-icons/ri';
+import { BiSolidMessageSquare } from 'react-icons/bi';
+
+
+
+
+const Nav = (props:any) => {
+    const width = useWindowDimensions().width;
+
+    return (
+        <div className={'flex flex-col w-full'}>
+            <div className='py-1'>
+                {props.app.user.isUser ?
+                    <>
+                        <NavBut text="Home" to="/user/home" icon={<RiComputerFill className='text-xl ml-2'/>} show={width > 700}/>
+                        <NavBut text="User" to={'/users?user='+props.app.user.name} icon={<FaUser className='text-xl ml-2'/>} show={width > 700}/>
+                        <NavBut text="Massages" to="/user/friends" icon={<BiSolidMessageSquare className='text-xl ml-2'/>} show={width > 700}/>
+                        <NavBut text="Groups" to="/user/groups" icon={<HiUserGroup className='text-xl ml-2'/>} show={width > 700}/>
+                    </>
+                    :
+                    <></>
+                }
+                <div className='mx-4 my-2 h-0.5 w-4/6'></div>
+
+                <NavBut text="Courses" to="/courses" icon={<HiAcademicCap className='text-xl ml-2'/>} show={width > 700}/>
+                {/*<NavBut text="Labs" to="/" icon={<BsServer className='text-xl'/>} show={width > 700}/>*/}
+
+                <div className='mx-4 my-2 h-0.5 w-4/6'></div>
+
+                <NavBut text="Pricing" to="/pricing" icon={<FaUserFriends className='text-xl ml-2'/>} show={width > 700}/>
+                <NavBut text="Contact" to="/contact" icon={<HiUserGroup className='text-xl ml-2'/>} show={width > 700}/>
+            </div>
+        </div>
+    );
+  }
+
+  export default Nav;
