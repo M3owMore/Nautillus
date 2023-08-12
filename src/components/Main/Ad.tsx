9ad5aa075b6d7c8364da49729b1906a80f051a20
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-12 20:40:40 +0400

    Delete src directory

--- src/components/Main/Ad.tsx
+++
@@ -1,9 +0,0 @@
-const Ad = () => {
-  return (
-    <div className='w-full px-4'>
-        <img src='./ad1.png' alt='ad' className='w-full rounded-2xl h-14 sm:h-24 md:h-32 object-cover'/>
-    </div>
-  )
-}
-
-export default Ad
