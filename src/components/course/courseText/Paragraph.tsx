a521709a36c8ee3e973fb2b6c9528a2906df0c84
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-13 21:11:00 +0400

    Delete src directory

--- src/components/course/courseText/Paragraph.tsx
+++
@@ -1,9 +0,0 @@
-const Paragraph = (props:any) => {
-    return (
-        <div className="text-main w-full flex font-arialGeo tracking-wide px-2">
-            {"ㅤ"}{props.children}
-        </div>
-    );
-}
-
-  export default Paragraph;
