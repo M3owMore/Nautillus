9bb5d92a50a67fdc2c8dd45a37035d276af64ff5
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-28 21:01:25 +0400

    Delete src directory

--- src/components/course/courseText/CMD.tsx
+++
@@ -1,9 +0,0 @@
-const CMD = (props:any) => {
-    return (
-        <div className="p-2 bg-bg-3 text-main rounded-2xl font-bpgESM my-1">
-            {" > "}{props.children}
-        </div>
-    );
-}
-
-  export default CMD;
--- src/components/course/courseText/Img.tsx
+++
@@ -1,9 +0,0 @@
-const Img = (props:any) => {
-    return (
-        <div className="w-full flex items-center justify-center">
-           <img src={props.src} className="max-w-lg w-full rounded-lg"/>
-        </div>
-    );
-}
-
-  export default Img;
