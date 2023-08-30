3acf23fbc433efb4528db8503ca07fe402080952
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-08-30 13:59:02 +0400

    Delete src directory

--- src/components/course/courseText/P.tsx
+++
@@ -1,9 +0,0 @@
-const P = (props:any) => {
-    return (
-        <div className="text-main w-full font-light tracking-wide px-2">
-            {"ㅤ"}{props.children}
-        </div>
-    );
-}
-
-  export default P;
