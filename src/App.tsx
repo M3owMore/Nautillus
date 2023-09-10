e8f7be2b70c3a868cec90f47c635a1d7b5eec819
sikha10 <111437894+sikha10@users.noreply.github.com>
2023-09-11 00:19:20 +0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,149 @@
+import Main from "./components/main/Main";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Course from "./components/course/Course";
+import Courses from "./components/Courses";
+import Pricing from "./components/Pricing";
+import Home from "./components/user/home/Home";
+import Activation from "./components/Activation";
+import Messages from "./components/user/friends/Messages";
+//import Groups from "./components/user/groups/Groups";
+import Contact from "./components/Contact";
+import CourseInfo from "./components/CourseInfo";
+import Header from "./components/Header";
+import ResetPassword from "./components/ResetPassword";
+import User from "./components/user/user/User";
+import ChangePassword from "./components/user/ChangePassword";
+import ChangeUsername from "./components/user/ChangeUsername";
+import ChangeProfilePicture from "./components/user/ChangeProfilePicture";
+import ChangeEmail from "./components/user/ChangeEmail";
+import Friends from "./components/user/Friends";
+import FriendRequests from "./components/user/FriendRequests";
+import SearchUsers from "./components/user/SearchUsers";
+import PayPalExecutePayment from "./components/PayPalExecutePayment";
+
+import langJson from "./langJson.json";
+import Cookies from 'universal-cookie';
+import ScrollOnTop from "./js/ScrollOnTop";
+import axios from "axios";
+
+
+const  App = () => {
+  const cookies = new Cookies();
+
+  if(cookies.get('lang') == null){
+    cookies.set('lang', 0, { path: '/', maxAge: 9999999999999})
+  }
+
+  const [lang, setLang] = useState(cookies.get('lang'))
+  const [user, setUser]:any = useState({
+    isUser: false,
+    canUseApp: 0
+  })
+
+
+
+  const backendURL = "http://127.0.0.1:8000/"
+  
+  const updateToken = async (getData:boolean) => {
+    console.log("Access token is updating...")
+    if(cookies.get('refresh') !== undefined && cookies.get('refresh') !== ""){
+      await axios.post(backendURL + "api/user/auth/jwt/refresh", {refresh: cookies.get('refresh')})
+      .then((res:any) => {
+        cookies.set('access', res.data.access, { path: '/',  maxAge: 180})
+        if(getData){
+          getUserInfo()
+        }
+        console.log("Access token is updated!")
+      })
+      .catch(() => {
+        setUser({
+          isUser: false,
+          canUseApp: -1
+        })
+        cookies.set('access', "", { path: '/' })
+        cookies.set('refresh', "", { path: '/' })
+      })
+    }else{
+      setUser({
+        isUser: false,
+        canUseApp: -1
+      })
+    }
+  };
+  const getUserInfo = async () => {
+    if(cookies.get('access') !== undefined && cookies.get('access') !== ""){
+      await axios.get(backendURL + "api/user/auth/users/me/", {headers: {Authorization: 'Bearer ' + cookies.get('access')}})
+        .then((res:any) => {
+          setUser({
+            isUser: true,
+            canUseApp: 1,
+            name: res.data.user_name,
+            img: `/userImages/${res.data.profile_picture}.webp`,
+            plan: "Free",
+            XP: 800,
+            level: "Beginner",
+          })
+        })
+        .catch(() => {
+          updateToken(true)
+        })
+    }else{
+      updateToken(false)
+    }
+  }
+  useEffect(() => {
+    updateToken(true)
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(updateToken, 180000, false);
+    return () => clearInterval(intervalId);
+  }, []); 
+
+  const app = {lang, setLang, langJson, cookies, backendURL, user, setUser};
+
+  return (
+    <BrowserRouter>
+      <Header app={app}/>
+      <ScrollOnTop/>
+      <Routes>
+        
+        {/*<Route path="/user/home" element={<Home app={app}/>}/>*/}
+        <Route path="/user/messages" element={<Messages app={app}/>}/>
+        {/*<Route path="/user/groups" element={<Groups app={app}/>}/>*/}
+        <Route path="/user/activation" element={<Activation app={app}/>}/>
+        <Route path="/user/forgotpass" element={<ResetPassword app={app}/>}/>
+        <Route path="/user/change/password" element={<ChangePassword app={app}/>}/>
+        <Route path="/user/change/name" element={<ChangeUsername app={app}/>}/>
+        <Route path="/user/change/profile" element={<ChangeProfilePicture app={app}/>}/>
+        <Route path="/user/change/email" element={<ChangeEmail app={app}/>}/>
+        <Route path="/user/friends" element={<Friends app={app}/>}/>
+        <Route path="/user/friends/requests" element={<FriendRequests app={app}/>}/>
+
+        
+        <Route path="/users/find" element={<SearchUsers app={app}/>}/>
+        <Route path="/users/user/:name" element={<User app={app}/>}/>
+
+        <Route path="/courses/course/:name" element={<Course app={app}/>}/>
+        <Route path="/courses/info/:name" element={<CourseInfo app={app}/>}/>
+
+        <Route path="/payment/execute/:token" element={<PayPalExecutePayment app={app}/>}/>
+
+
+        <Route path="/courses" element={<Courses app={app}/>}/>
+        <Route path="/pricing" element={<Pricing app={app}/>}/>
+        <Route path="/contact" element={<Contact app={app}/>}/>
+
+        <Route path="/signin" element={<SignIn app={app}/>}/>
+        <Route path="/signup" element={<SignUp app={app} />}/>
+        {user.canUseApp === 1 ? <Route path="/" element={<Home app={app}/>}/> : user.canUseApp === -1 ? <Route path="/" element={<Main app={app}/>}/> : null }
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/user/friends/FriendsList.tsx
@@ -0,0 +1,72 @@
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Friend = (props:any) => {
+    return ( 
+        <div className='w-full h-12 flex flex-row bg-zinc-900 my-1 p-1 text-main hover:bg-zinc-800 rounded-lg'
+            onClick={() => {
+                props.setUser(props.friend)
+                if(props.setIsChat != null){
+                    props.setIsChat(true)
+                }
+            }}
+        >
+            <img src={props.friend.img} className="w-10 h-10 rounded-full" alt="friend"/>
+            <div className='w-full h-full flex flex-col justify-center p-1 items-start pl-3'>
+                <p className="">{props.friend.name}</p>
+            </div> 
+        </div>
+    );
+}
+
+const FriendsList = (props:any) => {
+
+    const [frinds, setFriends]:any = useState([])
+
+    useEffect(() => {
+        
+        async function fetchData() {
+            const res = await axios.get(props.app.backendURL + "api/user/friends/", {headers: {
+                 Authorization: 'Bearer ' + props.app.cookies.get('access')
+            }});
+
+            setFriends(res.data)
+            if(res.data.length > 0){
+                let _friend = {
+                    name: res.data[0].user_name,
+                    img: `/userImages/${res.data[0].profile_picture}.webp`,
+                }
+                props.setUser(_friend)
+            }
+        };
+        
+        fetchData();
+    }, []);
+
+    return ( 
+        <div className='w-full h-full flex flex-col p-2'>  
+            <label htmlFor="simple-search" className="sr-only">Search</label>
+            <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                </div>
+                <input type="text" id="simple-search" className="bg-gray-900 border border-none text-main text-sm rounded-lg focus:border-none focus:outline-none block w-full pl-10 p-2.5 " placeholder="Search" required/>
+            </div>
+
+            <div className='h-full my-3 p-2 flex flex-col justify-start overflow-y-auto'>
+                {frinds.map((friend:any, i:any) => {
+                    let _friend = {
+                        name: friend.user_name,
+                        img: `/userImages/${friend.profile_picture}.webp`,
+                    }
+
+                    return <Friend setIsChat={props.setIsChat} setUser={props.setUser} key={i} friend={_friend} />
+            })}
+            </div>
+
+
+        </div>
+    );
+}
+ 
+export default FriendsList;
