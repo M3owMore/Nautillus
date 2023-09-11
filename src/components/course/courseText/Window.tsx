3e66787e8e2dcd880ac1e12e9d3d2eeddfe09fec
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-09-12 00:15:05 +0400

    Delete src directory

--- src/components/course/courseText/Window.tsx
+++
@@ -1,9 +0,0 @@
-const Window = (props:any) => {
-    return (
-        <div className=" text-main w-full max-w-2xl rounded-lg  text-md">
-            {props.children}
-        </div>
-    );
-}
-
-  export default Window;
