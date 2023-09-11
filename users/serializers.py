b6740821b8f081acb5750dc957b5d886a85e3181
sikha10 <111437894+sikha10@users.noreply.github.com>
2023-09-12 00:23:15 +0400

    Add files via upload

---
+++ users/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+from .models import UserCourse
+from courses.models import Course, CourseGroup
+from djoser.serializers import UserSerializer
+
+User = get_user_model()
+
+# class RegisterUserSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = NewUser
+#         fields = ('email', 'user_name', 'password')
+#         extra_kwargs = {'password': {'write_only': True}}
+
+#     def create(self, validated_data):
+#         password = validated_data.pop('password', None)
+#         instance = self.Meta.model(**validated_data)
+
+#         if password is not None:
+#             instance.set_password(password)
+#         instance.save()
+#         return instance
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'user_name', 'password')
+
+
+class CourseOpenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserCourse
+        fields = "__all__"
+
+
+class UserOpenCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseGroup
+        fields = "__all__"
+
+class ReturnLessonsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseGroup
+        fields = ('id', 'title', 'code', 'date_created')
+
+class ReturnUserSerializer(UserSerializer):    
+    class Meta(UserSerializer.Meta):
+        model = User
+        read_only_fields = ('email', 'profile_picture')
+        fields = ('id', 'email', 'user_name', 'profile_picture')
