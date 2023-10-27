1e579239a3caaa0a75d20aecdf02179a0ed561ff
sikha10 <111437894+sikha10@users.noreply.github.com>
2023-10-27 21:56:45 +0400

    Delete courses directory

--- courses/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Course
-from django.conf import settings
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ('id', 'title', 'level', 'description', 'big_description', 'topics', 'price', 'date_created')
