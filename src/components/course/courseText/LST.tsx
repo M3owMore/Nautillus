d93e4b8e8855cb77f22cdccb7ef3ea5638579112
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2023-10-27 21:53:56 +0400

    Delete src directory

--- src/components/course/courseText/LST.tsx
+++
@@ -1,9 +0,0 @@
-const LST = (props:any) => {
-    return (
-        <ol className="list-decimal m-1 p-2 px-6 bg-bg-3 rounded-lg">
-            {props.children}
-        </ol>
-    );
-}
-
-  export default LST;
