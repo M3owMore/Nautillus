347025fbf44348345e19221d8c7872299745ea6e
sikha10 <111437894+sikha10@users.noreply.github.com>
2023-11-11 21:25:09 +0400

    Delete blog directory

--- blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils import timezone 
-
-class Blog(models.Model):
-    title = models.TextField()
-    content = models.TextField()
-    date = models.DateTimeField(default=timezone.now)
