eac2f1089367aaf99d72f6c74f02b71d82ff99cd
Tengo Lomidze <88274495+tengolomidze@users.noreply.github.com>
2024-01-14 00:27:09 +0400

    Delete src directory

--- src/components/course/courseText/IMG.tsx
+++
@@ -1,9 +0,0 @@
-const IMG = (props:any) => {
-    return (
-        <div className="w-full flex items-center justify-center my-2">
-           <img src={props.src} className="w-full rounded-lg" alt={props.src}/>
-        </div>
-    );
-}
-
-  export default IMG;
