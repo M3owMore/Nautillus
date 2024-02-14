e000a0eec6e6f6a317a4ab5d6a2420f757525ea8
sikha10 <111437894+sikha10@users.noreply.github.com>
2024-02-14 23:37:31 +0400

    Update settings.py

--- nautillus/settings.py
+++ nautillus/settings.py
@@ -160,9 +160,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
 
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 SIMPLE_JWT = {
